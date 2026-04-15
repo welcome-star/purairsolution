f3284657bdf86b2b9c947520d1c74d156e35f115
welcome-star <welcome@digitis.agency>
2026-04-15 14:48:14 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 const createNextIntlPlugin = require("next-intl/plugin");
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
